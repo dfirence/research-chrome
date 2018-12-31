0a0caca95803b8b6433a2794adff1723a0ba5694
dfirence <carlos.diaz.pro@gmail.com>
2018-12-31 18:22:49 -0500

    Add Lab 5 - Native EVent Handlers

---
+++ codelab-extensions/lab-5-hello-world/README.md
@@ -0,0 +1,33 @@
+# Lab 3 - Hello World Extension w/ Native Javascript Event Listeners
+This extension demonstrates native web dev features to understand the usage of the architecture of a Chrome Extension.
+For example, we are using a standard javascript file called `popup.js` that is imported by the `popup.html` page.
+The event listeners are taken from native javascript eventhandlers with `.addEventListener()` that interacts
+with HTML5 buttons.
+
+This effectively demonstrates how to create a basic UI with buttons that reach to user actions when filling out
+the text input fields. As well as, providing the output to the user under the popup HTML view.
+
+
+# Functionality
+
+* Fill out 2 fields: First Name, Last Name
+* When the page is loaded without any input in the above fields, the native events auto fill value "john doe"
+* When the user wants to, they can RESET the values
+* When the user wants to, they can UPPERCASE the strings
+* When the user wants to, they can REVERSE the strings
+
+***
+
+# Key Takeaways
+* You can debug your Chrome Extension more effectively when developing it, by opening a new Chrome Browser TAB,
+  and typing the following in the URL Addresss Bar | chrome-extension://<your_extension_id/your_html_file.html>
+
+* When using `HTML INPUT` tags, the `innerHTML` attribute does not fill the values, instead you should try to use
+  the `document.getElementById('input').value = ...`
+
+* When setting up `Native JS Event Handlers`, try to use the global `document.addEventListener('DOMContentLoaded',()=>{})`
+
+* Remember, using `background.js` in a Chrome Extension is different than using `popup.js`
+
+* Remember, `browser_action` is a different page or UI experience than `page_action`, this lab is about learning how to
+  control the popup.html and popup.js
---
+++ codelab-extensions/lab-5-hello-world/js/popup.js
@@ -0,0 +1,69 @@
+/**
+ * This is not a background script.
+ *  This is a regular javascript used in the `popup.html` web page
+ *  This allows us to read the DOM elements in the `popup.html` web page
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    let firstName = document.getElementById('first-name');
+    let lastName = document.getElementById('last-name');
+    
+    if (firstName.value === "" && lastName.value === "") {
+        console.log("Nothing is typed, yet...")
+        // Now auto fill a value in the firstName
+        firstName.value = "john";
+        lastName.value = "doe";
+    }
+
+    // Use Output Box
+    // Let's auto fill the results from the input boxes
+    let user_output = document.getElementById('user-output');
+    user_output.innerHTML = firstName.value + " " + lastName.value;
+
+    // Add a Click Event Listener
+    // This binds it to the reset button in the `poopup.html` webpage
+    let reset_button = document.getElementById('reset');
+
+    reset_button.addEventListener('click', function() {
+        let firstName = document.getElementById('first-name');
+        let lastName = document.getElementById('last-name');
+        firstName.value = '';
+        lastName.value = '';
+        user_output.innerHTML = '';
+        console.log('Cleared Fields');
+    })
+
+    // Add a Click Event Listeners
+    // This binds it to the uppercase button in the `popup.html` webpage
+    let uppercase_button = document.getElementById('uppercase');
+
+    uppercase_button.addEventListener('click', function() {
+        if (firstName.value != "") {
+            firstName.value = firstName.value.toUpperCase();
+        }
+
+        if (lastName.value != "") {
+            lastName.value = lastName.value.toUpperCase();
+        }
+        user_output.innerHTML = firstName.value + " " + lastName.value;
+    })
+
+    // Add a Click Event Listener
+    // This binds it to the reverse button in the `popup.html` webpage
+    let reverse_button = document.getElementById('reverse');
+    reverse_button.addEventListener('click', function() {
+        if (firstName.value != "") {
+            let rv = firstName.value.split('').reverse();
+            firstName.value = rv.join('');
+        }
+
+        if (lastName.value != "") {
+            let rv = lastName.value.split('').reverse();
+            lastName.value = rv.join('');
+        }
+        user_output.innerHTML = firstName.value + " " + lastName.value;
+    })
+
+})
+
+
