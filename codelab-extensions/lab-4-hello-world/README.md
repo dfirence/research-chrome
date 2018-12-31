597bfce99bc37fe19aa77a38b048af242ae4f166
dfirence <carlos.diaz.pro@gmail.com>
2018-12-31 16:18:54 -0500

    Add Lab 4

---
+++ codelab-extensions/lab-4-hello-world/README.md
@@ -0,0 +1,15 @@
+# Lab 3 - Hello World Extension w/ TABS API
+This extension uses the Chrome TABS API to demonstrate its usage
+
+
+# Functionality
+The extension is intended to do a few things with tabs.
+
+* Allow the extension to  find inactive tabs and present the number of tabs opened under the badge
+
+***
+
+# Key Takeaways
+* The `chrome.tabs.query` requires a javascript object that is passed with API spec parameters
+* The results from the query return an array
+* Remember the event handlers in the background page are set as synchronous calls
---
+++ codelab-extensions/lab-4-hello-world/js/background.js
@@ -0,0 +1,28 @@
+// Create a function that queries and shows the number of tabs
+function queryAndShow() {
+    var tabNum = 0;
+    var query = { active: false };
+    chrome.tabs.query(query, function(tabs) {
+        if (tabs) {
+            tabNum = String(tabs.length);
+            chrome.browserAction.setBadgeText({text: String(tabNum)});
+            chrome.browserAction.setBadgeBackgroundColor({color: "#f44242"})
+        } 
+;
+    });
+}
+
+// Create a listener for when extension is installed
+chrome.runtime.onInstalled.addListener(function() {
+    queryAndShow();
+});
+
+// Create a listener for when a TAB is created
+// This should allow it to update after it is created
+chrome.tabs.onCreated.addListener(function() {
+    queryAndShow();
+});
+
+chrome.tabs.onRemoved.addListener(function() {
+    queryAndShow();
+});
